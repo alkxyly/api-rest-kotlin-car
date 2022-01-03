e977ad0d617dfd72e9a380bce32f91928724e6a1
alkxyly <alkxyly@hotmail.com>
2022-01-03 13:57:38 -0300

    Implementação de alguns novos recursos

--- src/main/kotlin/com/apicar/domain/Driver.kt
+++ src/main/kotlin/com/apicar/domain/Driver.kt
@@ -2,11 +2,14 @@ package com.apicar.domain
 
 import java.time.LocalDate
 import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
 import javax.persistence.Id
 
 @Entity
 data class Driver (
         @Id
+        @GeneratedValue
         var id: Long? = null,
         val name: String = "",
         val birthDate: LocalDate? = null
--- src/main/kotlin/com/apicar/interfaces/DriverAPI.kt
+++ src/main/kotlin/com/apicar/interfaces/DriverAPI.kt
@@ -2,18 +2,34 @@ package com.apicar.interfaces
 
 import com.apicar.domain.Driver
 import com.apicar.domain.DriverRepository
+import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.stereotype.Service
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
+import org.springframework.web.server.ResponseStatusException
 
 @Service
 @RestController
-@RequestMapping(produces = [MediaType.APPLICATION_JSON_VALUE])
+@RequestMapping("/drivers", produces = [MediaType.APPLICATION_JSON_VALUE])
 class DriverAPI (val driverRepository: DriverRepository){
 
-    @GetMapping("/drivers")
+    @GetMapping()
     fun listDrivers() : List<Driver> = driverRepository.findAll();
 
+    @GetMapping("/{id}")
+    fun findDriver(@PathVariable id: Long): Driver = driverRepository.findById(id)
+        .orElseThrow{ ResponseStatusException(HttpStatus.NOT_FOUND)}
+
+    @PostMapping()
+    fun createDriver(@RequestBody driver: Driver): Driver = driverRepository.save(driver);
+
+    @PutMapping("/{id}")
+    fun fullUpdateDriver(@PathVariable id: Long, @RequestBody driver: Driver): Driver {
+        val foundDriver: Driver = driverRepository.findById(id).orElseThrow();
+        val copyDriver = foundDriver.copy(
+            birthDate = driver.birthDate,
+            name =  driver.name
+        )
+        return driverRepository.save(copyDriver);
+    }
 }
